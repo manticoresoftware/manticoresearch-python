3e72ff7d8944b7925a8c3555147a00febdffbea3
nick <nick@manticoresearch.com>
2024-10-11 18:07:38 +0700

    Updated docs

--- manticoresearch/api/utils_api.py
+++ manticoresearch/api/utils_api.py
@@ -43,8 +43,8 @@ class UtilsApi:
     def sql(
         self,
         body: Annotated[StrictStr, Field(description="A query parameter string. ")],
-        raw_response: Annotated[Optional[StrictBool], Field(description="Optional parameter, defines a format of response. Can be set to `False` for Select only queries and set to `True` for any type of queries. Default value is 'True'. ")] = None,
-        mode: Annotated[Optional[StrictStr], Field(description="Optional parameter, defines a format of response. Can be set to empty for Select only queries and set to `raw` for any type of queries. Default value is 'raw'. ")] = None,
+        raw_response: Annotated[Optional[StrictBool], Field(description="Optional parameter, defines a format of response. Can be set to `False` for Select only queries and set to `True` for any type of queries. Default value is 'True'. ")] = True,
+        mode: Annotated[Optional[StrictStr], Field(description="Optional parameter, defines a format of response. Can be set to empty for Select only queries and set to `raw` for any type of queries. Default value is 'raw'. ")] = 'raw',
         _request_timeout: Union[
             None,
             Annotated[StrictFloat, Field(gt=0)],
