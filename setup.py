43f15e80dfc7f6b6bb0a1f73e18e3676d1ff13ac
nick <nick@manticoresearch.com>
2024-10-22 21:42:32 +0700

    Regenerated the client

--- setup.py
+++ setup.py
@@ -21,8 +21,8 @@ from setuptools import setup, find_packages  # noqa: H301
 #
 # prerequisite: setuptools
 # http://pypi.python.org/pypi/setuptools
-NAME = "manticoresearch"
-VERSION = "5.0.0"
+NAME = "manticoresearch-devel"
+VERSION = "5.0.0-dev"
 PYTHON_REQUIRES = ">=3.7"
 REQUIRES = [
     "urllib3 >= 1.25.3, < 2.1.0",
